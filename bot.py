1f5db205ae556b5e332437e28bac805cbe91905c
Jack Chen <jackchen010@yahoo.com>
2016-06-21 14:29:16 -0700

    Edited descriptions of bot

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def roll(dice="1d20"):
 
 
 
-@bot.command(description='For when you wanna settle the score some other way')
+@bot.command()
 async def choose(*choices : str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
@@ -122,7 +122,7 @@ async def logout():
     await bot.logout()
         
 
-@bot.command(pass_context = True, description = "A guessing game in which the player guesses a number from 1 to 10. Bot gives a provides which times out after 5 seconds of inactivity.")
+@bot.command(pass_context = True)
 async def guess(ctx):
     """A simple guessing game."""
     await bot.say("Okay, {}, guess a number between 1 to 10".format(ctx.message.author.name))
@@ -135,18 +135,24 @@ async def guess(ctx):
 
     guess = await bot.wait_for_message(timeout=5.0, author=ctx.message.author)
 
-    if guess is None:
+    """if guess is None:
         fmt = 'Sorry, you took too long. It was {}.'
         await bot.say(fmt.format(answer))
-        return
-    if int(guess.content) == answer:
-        await bot.say('You are right!')
-        txt_save("guessing_game_scoreboard.txt", ctx.message.author.name)
-    else:
-        await bot.say('Sorry. It is actually {}.'.format(answer))
+        return"""
+    try:
+        print(guess.content)
+        guess_int = int(guess.content)
+        print(guess_int)
+        if guess_int == answer:
+            await bot.say('You are right!')
+            txt_save("guessing_game_scoreboard.txt", ctx.message.author.name)
+        else:
+            await bot.say('Sorry. It is actually {}.'.format(answer))
+    except ValueError:
+        bot.say("That is not a number. Try again")
+        
 
-
-@bot.command(pass_context = True, description="The game john requires a system in which the player either has their maximum pool limit as either 7 or 10.")
+@bot.command(pass_context = True)
 async def john(ctx, proc: str):
     """A betting system for a game called John."""
     if proc == "start":
@@ -179,4 +185,4 @@ async def john(ctx, proc: str):
 
 
 #note that in order to run, the "token" must be replaced
-bot.run("token")
+bot.run("moodhtaed@outlook.com","BaconRocks42")
