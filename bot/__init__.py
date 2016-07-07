73c39fa504bfec82ce81ec18c4bdb4831a8cc53f
Jack Chen <jackchen@pilatus.ece.pdx.edu>
2016-07-07 15:22:47 -0700

    Added a folder containing documentation work

---
+++ bot/__init__.py
@@ -0,0 +1,18 @@
+"""DDbot is a simple bot written in Python using Rapptz's Discord API
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    bot
+
+Members
+=======
+"""
+
+from bot import pizza, guess, logout
+
+VERSION = "0.01"
+"""The version of this module."""
